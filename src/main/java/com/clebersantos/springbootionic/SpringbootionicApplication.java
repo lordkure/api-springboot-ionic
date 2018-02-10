91027d22b08bdba6120c5e2eb9c115fafaf335cb
Cleber Santos <santos.cleber@gmail.com>
2018-02-10 08:42:12 -0200

    Projeto criado

---
+++ src/main/java/com/clebersantos/springbootionic/SpringbootionicApplication.java
@@ -0,0 +1,12 @@
+package com.clebersantos.springbootionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootionicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootionicApplication.class, args);
+    }
+}
